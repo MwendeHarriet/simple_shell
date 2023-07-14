e38b61c2c08287d1ed235afe976b5e8abab9e5a0
MwendeHarriet <mwendeharriet@gmail.com>
2023-07-13 23:26:32 -0700

    readme&authors

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+#include <dirent.h>
+#include <stdarg.h>
+
+
+
+#endif
